1b9d8b96f2549e894bad514bfd816445c15f66b7
Дима <kitaykin9808@gmail.com>
2021-04-30 15:50:40 +0300

    Пункт заполнение с клавиатуры сдедал, но нужно править, комменты оставил

--- Auz_Project/Program.cs
+++ Auz_Project/Program.cs
@@ -79,7 +79,19 @@ namespace Auz_Project
                 Console.Write($" {ar3[i]} ");
             }
             Console.Write("}");
+            Console.WriteLine("\nНажмите на любую клавишу для расчета...");
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Копируем массив для сохранения
+        /// </summary>
+        /// <param массив до изменений="ar1"></param>
+        /// <returns>Копию</returns>
+        static double [] CopyArray(double[] ar1)
+        {
+            double[] ArrayCopy = ar1;
+            return ArrayCopy;
         }
 
         /// <summary>
@@ -208,6 +220,42 @@ namespace Auz_Project
             return null;
 
         }
+
+        static string SelectProject(double Gmax, double qj, double [] ar1, double [] ar2, double [] ar3)
+        {
+            double[] arE1 = ar1;
+            double[] arE2 = ar2;
+            double[] arE3 = ar3;
+            string E1 = "E1 эффективнее";
+            string E2 = "E2 эффективнее";
+            string E3 = "E3 эффективнее";
+            string error = "Error";
+
+            for (int i = 0; i < arE1.Length; i++)
+            {
+                arE1[i] = arE1[i] * qj;
+                Math.Round(arE1[i], 2);
+                if (arE1[i] == Gmax)
+                    return E1;
+            }
+            for (int i = 0; i < arE2.Length; i++)
+            {
+                arE2[i] = arE2[i] * qj;
+                Math.Round(arE2[i], 2);
+                if (arE2[i] == Gmax)
+                    return E2;
+            }
+            for (int i = 0; i < arE3.Length; i++)
+            {
+                arE3[i] = arE3[i] * qj;
+                Math.Round(arE3[i], 2);
+                if (arE3[i] == Gmax)
+                    return E3;
+            }
+
+            return error;
+
+        }
         static void InputProject(string Project)
         {
             Console.WriteLine("\nПриходим к выводу что {0}",Project); Console.ReadKey();
@@ -235,14 +283,26 @@ namespace Auz_Project
                     Three(Gmax);
                     string Project = SelectProject(Gmax, qj);
                     InputProject(Project);
+
                     break;
 
-                case "2":
-                    double[] e1 = new double [3]; FillArray(e1); 
+                case "2":                                   // ввод параметров от пользователя
+
+                    double[] e1 = new double [3]; FillArray(e1);
                     double[] e2 = new double [3]; FillArray(e2); 
                     double[] e3 = new double [3]; FillArray(e3);
                     double qj2 = InputQJ();
+                    double[] e1Copy = CopyArray(e1);
+                    double[] e2Copy = CopyArray(e2);
+                    double[] e3Copy = CopyArray(e3);
                     OutputArray(e1,e2,e3);
+                    One(e1, e2, e3, qj2);
+                    double[] Gmin2 = Two(e1, e2, e3);
+                    Two(Gmin2);
+                    double Gmax2 = Three(Gmin2);
+                    Three(Gmax2);
+                    string Project2 = SelectProject(Gmax2, qj2, e1Copy, e2Copy, e3Copy);
+                    InputProject(Project2);
 
                     break;
 
@@ -260,7 +320,8 @@ namespace Auz_Project
             Осталось выполнить:
             1. Минимаксный критерий
             2. Возможность ввода параметров от пользователя +
-            3. Сделать рассчет параметров от пользователя
+            3. Сделать рассчет параметров от пользователя +/- (Реализовать ref/out к варианту 2.: ввод данных )
+            4. Проблема при выборе лучшего проекта, разобратся 
              */
         }
     }
